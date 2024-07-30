cd8d47ff56787b276703486cd6816f5eed02ed0c
Ryan Guinchard <ryanguinchard9@hotmail.com>
2024-07-30 21:09:46 -0230

    React router setup and got basic directory started

---
+++ FrontEnd/src/pages/Main.jsx
@@ -0,0 +1,13 @@
+const Main = () => {
+  return (
+    <>
+      <div className="main-page">
+        <h1>Welcome to DecisionForge</h1>
+        <p>Get started by selecting a game to randomize your choices.</p>
+        {/* Placeholder yall. Will be updated with a list of games that we will fetch from db */}
+      </div>
+    </>
+  );
+};
+
+export default Main;
